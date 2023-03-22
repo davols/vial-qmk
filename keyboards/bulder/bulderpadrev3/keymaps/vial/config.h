7511ba27bfeb6eb135e3bd7d0ae54a3c15bf06bb
David Olsson <david@davido.me>
2023-03-22 14:08:46 +0100

    Bulderpad

---
+++ keyboards/bulder/bulderpadrev3/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x76, 0x10, 0x08, 0x2E, 0x33, 0x78, 0x46, 0x7E}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
